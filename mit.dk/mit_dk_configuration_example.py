bc124e94ec7408afe77f76ac7ad59b40dc4d7023
n0kovo <n0kovo@riseup.net>
2023-04-26 16:48:50 +0200

    Fix syntax error in example config

--- mit.dk/mit_dk_configuration_example.py
+++ mit.dk/mit_dk_configuration_example.py
@@ -2,7 +2,7 @@ tokens_filename = 'mit_dk_tokens.json'
 
 email_data = {
     'emailserver': '', 	    # Your mail server hostname, e.g. host.server.dk
-    'emailserverport': ,	# Mail server port, e.g. 465
+    'emailserverport': '',	# Mail server port, e.g. 465
     'emailusername': '',	# Sender mail account username
     'emailpassword': '',	# Sender mail account password
     'emailfrom': '',		# Sender e-mail, e.g. trump@usa.gov
